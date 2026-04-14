037929cde3994c2db443219b2b6fea21d317d596
vanikrishna-dev <vanikrishna02@gmail.com>
2026-04-14 08:07:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,15 +2,21 @@ const getSumBtn = document.createElement("button");
 getSumBtn.append("Get Total Price");
 document.body.appendChild(getSumBtn);
 
-prices.forEach(item => {
-    let value = item.innerText.trim();
+const getSum = () => {
+	let totalPrice = 0;
 
-    let nums = value.match(/\d+/g);
+	const prices = document.querySelectorAll(".prices");
 
-    let finalValue = nums ? nums[nums.length - 1] : 0;
+	prices.forEach(item => {
+        let value = item.innerText.trim();
 
-    totalPrice += parseInt(finalValue);
-});
+        let nums = value.match(/\d+/g);
+        let finalValue = nums ? nums[nums.length - 1] : 0;
+
+        totalPrice += parseInt(finalValue);
+    });
+
+	document.getElementById("ans").innerText = totalPrice;
+};
 
 getSumBtn.addEventListener("click", getSum);
-
